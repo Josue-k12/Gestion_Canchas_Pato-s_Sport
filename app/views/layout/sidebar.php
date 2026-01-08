39cf8931a4d5d205dda5e8f32e5f5861fa153010
ERICK <erickcace81@gmail.com>
2026-01-08 16:31:21 -0500

    Arquitectura MVC: Archivos organizados en carpetas app y public, y agregados activos de AdminLTE

---
+++ app/views/layout/sidebar.php
@@ -0,0 +1,19 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="index.php" class="brand-link">
+        <img src="public/img/logo_patos.png" alt="Logo" class="brand-image img-circle elevation-3">
+        <span class="brand-text font-weight-light">Pato's Sport</span>
+    </a>
+
+    <div class="sidebar">
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" role="menu">
+                <li class="nav-item">
+                    <a href="index.php?c=Home&a=index" class="nav-link">
+                        <i class="nav-icon bi bi-house"></i>
+                        <p>Inicio</p>
+                    </a>
+                </li>
+                </ul>
+        </nav>
+    </div>
+</aside>
