d1ab4671a50ade4bc3d0c5570d96988912d87807
ERICK <erickcace81@gmail.com>
2026-01-08 17:09:41 -0500

    Arquitecto: Configuración base y carpeta database habilitada

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('URL', 'http://localhost/Gestion_Canchas_Pato-s_Sport/');
+define('HOST', 'localhost');
+define('DB', 'gestion_canchas');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+?>
