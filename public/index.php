d93bd94c1d968a23ed30c0547aa4b0c167122d2b
ERICK <erickcace81@gmail.com>
2026-01-15 12:39:26 -0500

    Fix: Ruta de configuración corregida en index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
-// 1. Cargamos la configuración (Subimos un nivel con ../ para encontrar la carpeta config)
-require_once '../config/config.php';
+// 1. Cargamos la configuración 
+// (Entramos a ../app/config/ porque config está dentro de app)
+require_once '../app/config/config.php';
 
-// 2. Cargamos la vista del home (Subimos un nivel para entrar a app/views)
+// 2. Cargamos la vista del home
 include '../app/views/home/index.php';
 ?>
